9e2025178a5b21cfaaf4bedd4c2efedcc941d354
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2023-03-16 00:39:38 +0400

    debugging js file name

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,22 +1,16 @@
 alert("💖🥳 Cheers to you, and here's to making 2023 your best year yet!🎂🎉 Celebrate and be happy.!🎇🧨");
-function displayBirthdayMessage() {
-  const xhr = new XMLHttpRequest();
+async function displayBirthdayMessage() {
+  try {
+    const response = await fetch('/assets/json/messages.json');
+    const data = await response.json();
 
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      const data = JSON.parse(xhr.responseText);
+    // Get a random message from the array
+    const randomIndex = Math.floor(Math.random() * data.length);
+    const randomMessage = data[randomIndex].text;
 
-      // Get a random message from the array
-      const randomIndex = Math.floor(Math.random() * data.length);
-      const randomMessage = data[randomIndex].text;
-
-      // Display the message
-      alert(randomMessage);
-    } else if (xhr.readyState === 4) {
-      console.error('Failed to load messages');
-    }
-  };
-
-  xhr.open('GET', '/assets/json/messages.json');
-  xhr.send();
+    // Display the message
+    alert(randomMessage);
+  } catch (error) {
+    console.error(error);
+  }
 }
