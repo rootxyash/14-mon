beb9e7189b3a6c76416ee90f27ab51777d6a37a9
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2023-03-16 00:07:20 +0400

    updating json with js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,16 @@
 alert("💖🥳 Cheers to you, and here's to making 2023 your best year yet!🎂🎉 Celebrate and be happy.!🎇🧨");
-function hang() {
-  alert(
-    "❣Happy Birthday! Wishing you a year filled with joy, love, and unforgettable moments. As you embark on another trip around the sun, may you be blessed with good health, success in all your endeavors, and endless opportunities for growth. "
-  );
+async function displayBirthdayMessage() {
+  try {
+    const response = await fetch('/assets/json/messages.json');
+    const data = await response.json();
+
+    // Get a random message from the array
+    const randomIndex = Math.floor(Math.random() * data.length);
+    const randomMessage = data[randomIndex].text;
+
+    // Display the message
+    alert(randomMessage);
+  } catch (error) {
+    console.error(error);
+  }
 }
