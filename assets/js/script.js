9b404018d64afaa48bd0f738e9776781bdadd561
Sushil Sharma <58214222+thesushilsharma@users.noreply.github.com>
2023-03-15 16:50:12 +0400

    updated format

---
+++ assets/js/script.js
@@ -0,0 +1,6 @@
+alert("💖🥳 Cheers to you, and here's to making 2023 your best year yet!🎂🎉 Celebrate and be happy.!🎇🧨");
+function hang() {
+  alert(
+    "❣Happy Birthday! Wishing you a year filled with joy, love, and unforgettable moments. As you embark on another trip around the sun, may you be blessed with good health, success in all your endeavors, and endless opportunities for growth. "
+  );
+}
